06cd8b2741eb0550bca2c48a0d582fd4afb786f8
jayjaycaneda52-spec <jayjaycaneda52@gmail.com>
2026-04-21 12:59:18 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,65 +1,65 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('Bot is alive!'));
-app.listen(3000, () => console.log('Port :3000 opened'));
+const mineflayer = require('mineflayer')
+const express = require('express')
+const fs = require('fs')
 
-const mineflayer = require('mineflayer');
-const { pathfinder, Movements, goals } = require('mineflayer-pathfinder');
-const settings = require('./settings.json');
+// Load settings.json
+const settings = JSON.parse(fs.readFileSync('./settings.json', 'utf8'))
 
-const bot = mineflayer.createBot({
-  host: settings.server.ip,
-  port: settings.server.port,
-  username: settings.bot.username,
-  version: settings.server.version,
-  auth: settings.bot.auth
-});
-
-bot.loadPlugin(pathfinder);
-
-bot.on('spawn', () => {
-  console.log('[BotLog] Bot has spawned');
-  if (settings.utils['auto-auth'].enabled) {
-    setTimeout(() => {
-      bot.chat(`/register ${settings.utils['auto-auth'].password} ${settings.utils['auto-auth'].password}`);
-      bot.chat(`/login ${settings.utils['auto-auth'].password}`);
-    }, 500);
-  }
-});
-
-bot.on('kicked', (reason) => {
-  console.log('[BotLog] Bot was kicked. Reason:', reason);
-  if (settings['auto-reconnect']) {
-    setTimeout(() => process.exit(), settings['auto-reconnect-delay'] * 1000);
-  }
-});
-
-bot.on('error', (err) => {
-  console.log('[BotLog] Bot error:', err);
-});
-
-bot.on('end', () => {
-  console.log('[BotLog] Bot disconnected');
-  if (settings['auto-reconnect']) {
-    setTimeout(() => process.exit(), settings['auto-reconnect-delay'] * 1000);
-  }
-});
-
-if (settings['chat-messages'].enabled) {
-  setInterval(() => {
-    const msg = settings['chat-messages'].messages[Math.floor(Math.random() * settings['chat-messages'].messages.length)];
-    bot.chat(msg);
-  }, settings['chat-messages']['repeat-delay'] * 1000);
-}
-
-if (settings['chat-log']) {
-  bot.on('chat', (username, message) => {
-    console.log(`[ChatLog] <${username}> ${message}`);
-  });
-}
-// Keep alive para kay Render
-const express = require('express');
+// Keep alive server para kay Render - ISA LANG TO
 const app = express();
 app.get('/', (req, res) => res.send('Jay_afk bot is running!'));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Fake web server running on ${PORT}`));
+
+// Gawa ng bot
+const bot = mineflayer.createBot({
+  host: settings.host,
+  port: settings.port,
+  username: settings.username,
+  version: settings.version
+})
+
+// Bot events
+bot.on('spawn', () => {
+  console.log('[BotLog] Bot spawned!')
+  console.log(`[BotLog] Logged in as ${bot.username}`)
+})
+
+bot.on('login', () => {
+  console.log('[BotLog] Bot logged in to server')
+})
+
+bot.on('kicked', (reason) => {
+  console.log('[BotLog] Bot kicked:', reason)
+})
+
+bot.on('error', (err) => {
+  console.log('[BotLog] Bot error:', err)
+})
+
+bot.on('end', () => {
+  console.log('[BotLog] Bot disconnected')
+  if (settings['auto-reconnect']) {
+    console.log('[BotLog] Reconnecting in 5s...')
+    setTimeout(() => process.exit(), 5000)
+  }
+})
+
+// Auto-chat kung naka-enable sa settings.json
+if (settings['chat-messages'] && settings['chat-messages'].enabled) {
+  setInterval(() => {
+    const msg = settings['chat-messages'].messages[Math.floor(Math.random() * settings['chat-messages'].messages.length)]
+    bot.chat(msg)
+    console.log(`[BotLog] Sent message: ${msg}`)
+  }, settings['chat-messages']['repeat-delay'] * 1000)
+}
+
+// Chat log kung naka-enable
+if (settings['chat-log']) {
+  bot.on('chat', (username, message) => {
+    if (username === bot.username) return
+    console.log(`[ChatLog] <${username}> ${message}`)
+  })
+}
+
+console.log('[BotLog] Starting bot...')
