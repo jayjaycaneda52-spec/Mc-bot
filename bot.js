0e26c1fa9edf01080b8fa249494bcf37ace686e5
jayjaycaneda52-spec <jayjaycaneda52@gmail.com>
2026-05-05 10:18:13 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,7 +43,39 @@ function createBot() {
     setTimeout(() => createBot(), 40000)
   })
 }
+bot.on('spawn', () => {
+  console.log('[BotLog] Bot connected')
 
+  // AUTO LOGIN/REGISTER
+  setTimeout(() => {
+    bot.chat('/register 123456 123456')
+    console.log('[BotLog] Sent register command')
+  }, 2000)
+
+  setTimeout(() => {
+    bot.chat('/login 123456')
+    console.log('[BotLog] Sent login command')
+  }, 5000)
+
+  // ANTI-AFK: Talon every 20 seconds
+  setInterval(() => {
+    bot.setControlState('jump', true)
+    setTimeout(() => bot.setControlState('jump', false), 500)
+    console.log('[BotLog] Anti-AFK: jump')
+  }, 20000)
+
+  // ANTI-AFK: Palinga-linga every 15 seconds
+  setInterval(() => {
+    bot.look(Math.random() * Math.PI * 2, 0)
+    console.log('[BotLog] Anti-AFK: look around')
+  }, 15000)
+
+  // AUTO LEAVE EVERY 10 MINS - DAGDAG MO TO
+  setTimeout(() => {
+    console.log('[BotLog] 10 minutes na, disconnect na ako')
+    bot.quit('10 min restart')
+  }, 10 * 60 * 1000) // 10 mins = 600000ms
+})
 createBot()
 
 // KEEP ALIVE PARA DI MATULOG SA RENDER
